97bd4cd9f16dafbd09a99b1f6da152df0c52b4cd
Sergey Goncharuk <fleshed914@gmail.com>
2021-08-17 15:14:45 +0300

    feat: add Morse decode()

--- src/index.js
+++ src/index.js
@@ -37,10 +37,53 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+const decode = expr => {
+    let part = '';
+    let stringArr = [];
+    let stringMorse = '';
+    let morseArr = [];
+    let encodedStr = '';
+
+
+    for(let j = 0; j < expr.length; j = j + 10) {
+        part = expr.slice(j, j + 10);
+        stringArr.push(part);
+    }
+        
+    for(let i = 0; i < stringArr.length; i++) {
+
+        let elemArr = [];
+
+        for(let j = 0; j < stringArr[i].length; j = j + 2) {
+            
+            stringMorse = stringArr[i].slice(j, j + 2);
+
+            if(stringMorse == '11') elemArr.push('-');
+            if(stringMorse == '10') elemArr.push('.');
+            if(stringMorse == '**' && elemArr.indexOf('**') == -1) elemArr.push('**');
+     
+        }
+        
+        if(elemArr.length != 0) {
+            morseArr.push(elemArr.join(''));
+        }
+    }
+
+    for(let i = 0; i < morseArr.length; i++) {
+        
+        if(morseArr[i] == '**') {
+            encodedStr += ' ';
+        } else {
+            encodedStr += MORSE_TABLE[morseArr[i]];
+        }  
+    }
+    
+    return encodedStr;
 }
+
 
 module.exports = {
     decode
 }
+
+
